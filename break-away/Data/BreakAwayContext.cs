783bf7ff0efc34a0dda970b160f80c6086b2bc50
chris.han <namilhan@gmail.com>
2023-05-28 13:54:59 +0900

    add OnModelCreating Method

--- break-away/Data/BreakAwayContext.cs
+++ break-away/Data/BreakAwayContext.cs
@@ -1,5 +1,6 @@
 namespace break_away.Data;
 
+using System.Reflection;
 using break_away.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,4 +12,20 @@ public class BreakAwayContext: DbContext
     
     public DbSet<Destination> Destinations { get; set; }
     public DbSet<Lodging> Lodgings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Destination>()
+                    .Property(d => d.Name)
+                    .IsRequired()
+                    .HasMaxLength(200);
+        modelBuilder.Entity<Destination>()
+                    .Property(d => d.Description)
+                    .HasMaxLength(500);
+        
+        modelBuilder.Entity<Lodging>()
+                    .Property(l => l.Name)
+                    .IsRequired()
+                    .HasMaxLength(200);
+    }
 }
---
+++ break-away/Migrations/20230528045338_AddOnModelCreatingMethodToBreakAwayContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace break_away.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOnModelCreatingMethodToBreakAwayContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lodgings",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Destinations",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lodgings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
